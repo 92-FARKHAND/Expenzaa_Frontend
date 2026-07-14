a5daf755ba38b6280c047fbdfd550dd87df54a6b
Farkhand92 <farkhandwaqar786@gmail.com>
2026-07-15 00:38:44 +0500

    Creating .env file

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  server:{
-   proxy:{
-    '/api':'http://localhost:5000'
-   }
-  },
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    server: {
+      proxy: {
+        '/api': {
+          target: env.API_URL,
+          changeOrigin: true,
+          secure: true,
+        }
+      }
+    },
+    plugins: [
+      react(),
+      tailwindcss()
+    ],
+  }
 })
